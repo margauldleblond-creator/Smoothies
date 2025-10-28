355209a0587e86630f41f000748ce37794a3e946
margauldleblond-creator <margauld.leblond@gmail.com>
2025-10-28 09:25:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,8 +52,7 @@ if ingredient_liste :
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
-            values ('""" + ingredients_string + """','""" + name_of_order + """')"""
+my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER) values ('""" + ingredients_string + """','""" + name_of_order + """')"""
 
 #st.write(my_insert_stmt)
 #st.stop()
